db901f40d6af4ab7c892790142d12c36629116bb
Mihael Isaev <isaev.mihael@gmail.com>
2013-08-14 18:36:47 +0400

    Initial Commit

---
+++ InMac Chat/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  InMac Chat
+//
+//  Created by mihael on 14.08.13.
+//  Copyright (c) 2013 Mihael Isaev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
